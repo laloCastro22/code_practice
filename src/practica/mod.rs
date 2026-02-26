60222397acae15d9c0c276ea61dd9db23ae4879d
Eduardo <eduardo.castro@syesoftware.com>
2026-02-26 16:09:37 -0600

    FEAT:   - Adicion de metodos a los modulos

--- src/practica/mod.rs
+++ src/practica/mod.rs
@@ -1,7 +1,9 @@
 mod ejercicio1;
 mod ejercicio2;
 mod ejercicio3;
+mod ejercicio4;
 
 pub use self::ejercicio1::{TreeNode, sum_root_to_leaf_preorden};
 pub use self::ejercicio2::sort_by_bits;
 pub use self::ejercicio3::two_sum;
+pub use self::ejercicio4::num_steps;
