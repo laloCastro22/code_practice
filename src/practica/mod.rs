55ae94a531c379eec0e3f763b028323b39d9bd0b
Eduardo <eduardo.castro@syesoftware.com>
2026-02-27 15:03:36 -0600

    FEAT: add ejercicio5 module and expose min_operations function

--- src/practica/mod.rs
+++ src/practica/mod.rs
@@ -2,8 +2,10 @@ mod ejercicio1;
 mod ejercicio2;
 mod ejercicio3;
 mod ejercicio4;
+mod ejercicio5;
 
 pub use self::ejercicio1::{TreeNode, sum_root_to_leaf_preorden};
 pub use self::ejercicio2::sort_by_bits;
 pub use self::ejercicio3::two_sum;
 pub use self::ejercicio4::num_steps;
+pub use self::ejercicio5::min_operations;
