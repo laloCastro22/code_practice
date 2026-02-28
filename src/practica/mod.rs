6407268d4c16a5b963a567c242a7e97c620af2e7
Eduardo <eduardo.castro@syesoftware.com>
2026-02-28 14:55:17 -0600

    FEAT:  - Adicion de modulos de ejecicio 3

--- src/practica/mod.rs
+++ src/practica/mod.rs
@@ -4,6 +4,7 @@ mod ejercicio3;
 mod ejercicio4;
 mod ejercicio5;
 mod ejercicio6;
+mod ejercicio7;
 
 pub use self::ejercicio1::{TreeNode, sum_root_to_leaf_preorden};
 pub use self::ejercicio2::sort_by_bits;
@@ -11,3 +12,4 @@ pub use self::ejercicio3::two_sum;
 pub use self::ejercicio4::num_steps;
 pub use self::ejercicio5::min_operations;
 pub use self::ejercicio6::{add_two_numbers, ListNode};
+pub use self::ejercicio7::concatenated_binary;
