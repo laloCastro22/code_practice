f145e4711d4fa539e5fccdccbd57f6c6524e6ad9
Eduardo <eduardo.castro@syesoftware.com>
2026-03-02 11:04:37 -0600

    FEAT:  - Cambio en los modulos

--- src/practica/mod.rs
+++ src/practica/mod.rs
@@ -5,6 +5,7 @@ mod ejercicio4;
 mod ejercicio5;
 mod ejercicio6;
 mod ejercicio7;
+mod ejercicio8;
 
 pub use self::ejercicio1::{TreeNode, sum_root_to_leaf_preorden};
 pub use self::ejercicio2::sort_by_bits;
@@ -13,3 +14,4 @@ pub use self::ejercicio4::num_steps;
 pub use self::ejercicio5::min_operations;
 pub use self::ejercicio6::{add_two_numbers, ListNode};
 pub use self::ejercicio7::concatenated_binary;
+pub use self::ejercicio8::min_swaps;
